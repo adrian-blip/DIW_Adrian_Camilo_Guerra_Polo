2406095c31e54a43f1811afd8476267cc5e10b8b
adrian-blip <adriancamiloguerra@gmail.com>
2025-02-18 13:44:29 +0100

    update

---
+++ Fuente/scripts/js/menuScript.js
@@ -0,0 +1,15 @@
+// Selección de elementos
+const menu = document.querySelector(".nav-menu");
+const openBtn = document.querySelector(".menu-toggle");
+const closeBtn = document.querySelector(".close-btn");
+
+// Función para abrir el menú
+openBtn.addEventListener("click", () => {
+  menu.classList.add("active"); // Añadir clase 'active' para mostrar el menú
+});
+
+// Función para cerrar el menú
+closeBtn.addEventListener("click", () => {
+  menu.classList.remove("active"); // Quitar clase 'active' para ocultar el menú
+});
+  
